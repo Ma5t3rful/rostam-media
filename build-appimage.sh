e5619ba06b6b7e08ef7965f17a0bcd166fe6cf14
Ma5t3rful <37829730+Ma5t3rful@users.noreply.github.com>
2026-02-22 23:09:53 +0330

    Remove old script

--- build-appimage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -fr AppDir/
-cmake -B build/ -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=AppDir/usr 
-cmake --install build/ --config Release # main executable, icons and desktop file will be installed by cmake
-linuxdeploy --appdir AppDir/  --output appimage
