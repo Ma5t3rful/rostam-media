dcebac49c8d93c95fc56a31530e2264a58c9c5a3
Ma5t3rful <37829730+Ma5t3rful@users.noreply.github.com>
2026-04-15 14:35:13 +0330

    Set window size limits

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,10 @@ auto main (int argc, char** argv) -> int
     });
     glfwInit();
     auto* window = glfwCreateWindow(1000, 500, "Rostam Media", nullptr, nullptr);
+    glfwSetWindowSizeLimits(window, 280, 250, GLFW_DONT_CARE, GLFW_DONT_CARE);
     glfwMakeContextCurrent(window);
     //if(const auto theme = std::getenv("ROSTAM_THEME");theme != nullptr)
-    
+
     const auto possible_theme_paths = std::to_array({cross_platform::exe_path()/"dark.txt",std::filesystem::path("/usr/share/rostam_theme/dark.txt")});
     if(const auto found = std::ranges::find_if(possible_theme_paths,[](const auto& p){return std::filesystem::exists(p);});
     found != possible_theme_paths.cend())
